5670d3043876b69a4a80f8909632db54c38262f5
CarlosMH95 <cmanosalvas95@gmail.com>
2017-04-23 15:25:38 -0500

    modulo PDF

--- DeberPDFs/PDF/views.py
+++ DeberPDFs/PDF/views.py
@@ -1,2 +1,30 @@
+from reportlab.pdfgen import canvas
+from django.http import HttpResponse
+from reportlab.lib.units import inch
+from io import BytesIO
+
 def get_pdf(request,nombreEmpresa,nombrePersona):
-    pass
+    # Create the HttpResponse object with the appropriate PDF headers.
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = 'attachment; filename="somefilename.pdf"'
+
+    buffer = BytesIO()
+
+    # Create the PDF object, using the BytesIO object as its "file."
+    p = canvas.Canvas(buffer)
+
+    # Draw things on the PDF. Here's where the PDF generation happens.
+    # See the ReportLab documentation for the full list of functionality.
+    p.drawString(100, 600, "%s."%nombreEmpresa)
+    p.drawString(100, 500, "%s."%nombrePersona)
+
+    # Close the PDF object cleanly.
+    p.showPage()
+    p.save()
+
+    # Get the value of the BytesIO buffer and write it to the response.
+    pdf = buffer.getvalue()
+    buffer.close()
+    response.write(pdf)
+    return response
+    
